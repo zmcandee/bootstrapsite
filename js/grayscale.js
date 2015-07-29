7e9e1d946301598a4d1176261b436701820968c6
Zach Candee <zcandee@xes-inc.com>
2015-07-29 11:21:37 -0500

    expand tracking to all links

--- js/grayscale.js
+++ js/grayscale.js
@@ -15,7 +15,7 @@ $(window).scroll(function() {
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
+    $('a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
